a4d9a2ca227dc5eb6e2bb26d282fbbf8d9ac9a67
= <=>
2020-07-22 14:14:16 +0700

    Add FunctionConfiguration

---
+++ MYUCoreApp.Data.EF/Configurations/FunctionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TeduCoreApp.Data.Entities;
+using static MYUCoreApp.Data.EF.Extensions.ModelBuilderExtensions;
+
+namespace MYUCoreApp.Data.EF.Configurations
+{
+    public class FunctionConfiguration : DbEntityConfiguration<Function>
+    {
+        public override void Configure(EntityTypeBuilder<Function> entity)
+        {
+            entity.HasKey(c=>c.Id);
+            entity.Property(c => c.Id).IsRequired().HasColumnType("varchar(128)");
+        }
+    }
+}
